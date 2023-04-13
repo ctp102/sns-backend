903b33ba9da8d3a81ed6e0720cf2d8168ecfdaec
jh.cho <cjh951002@gmail.com>
2023-04-13 23:58:49 +0900

    build: lombok(test code), querydsl 의존성 추가

--- src/test/java/world/meta/sns/SnsApplicationTests.java
+++ src/test/java/world/meta/sns/SnsApplicationTests.java
@@ -1,14 +1,17 @@
 package world.meta.sns;
 
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
+@Slf4j
 class SnsApplicationTests {
 
     @Test
     void contextLoads() {
         System.out.println("Hello World!");
+        log.info("Hello World!");
     }
 
 }
