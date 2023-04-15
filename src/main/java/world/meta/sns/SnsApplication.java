96a34111e9bae1308be868ae43d4273adfeac19e
jh.cho <cjh951002@gmail.com>
2023-04-15 16:51:52 +0900

    feat: 초기 데이터 설정 추가

--- src/main/java/world/meta/sns/SnsApplication.java
+++ src/main/java/world/meta/sns/SnsApplication.java
@@ -1,13 +1,36 @@
 package world.meta.sns;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.event.EventListener;
+import world.meta.sns.entity.Board;
+import world.meta.sns.entity.Member;
+import world.meta.sns.repository.BoardRepository;
+import world.meta.sns.repository.MemberRepository;
 
 @SpringBootApplication
+@RequiredArgsConstructor
 public class SnsApplication {
+
+    private final MemberRepository memberRepository;
+    private final BoardRepository boardRepository;
 
     public static void main(String[] args) {
         SpringApplication.run(SnsApplication.class, args);
+    }
+
+    @EventListener(ApplicationReadyEvent.class)
+    public void init() {
+
+        // member
+        Member member = new Member("member1");
+        memberRepository.save(member);
+
+        // board
+        Board board = new Board("title1", "content1", member);
+        boardRepository.save(board);
     }
 
 }
