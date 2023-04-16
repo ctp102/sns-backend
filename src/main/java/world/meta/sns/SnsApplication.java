08e6d2054cf9c9773dec4b2ee6114f599f6ffdb5
jh.cho <cjh951002@gmail.com>
2023-04-16 23:53:21 +0900

    chore: 초기 데이터 설정 수정

--- src/main/java/world/meta/sns/SnsApplication.java
+++ src/main/java/world/meta/sns/SnsApplication.java
@@ -6,9 +6,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.event.EventListener;
 import world.meta.sns.entity.Board;
+import world.meta.sns.entity.Comment;
 import world.meta.sns.entity.Member;
 import world.meta.sns.enums.Category;
 import world.meta.sns.repository.board.BoardRepository;
+import world.meta.sns.repository.comment.CommentRepository;
 import world.meta.sns.repository.member.MemberRepository;
 
 @SpringBootApplication
@@ -17,6 +19,7 @@ public class SnsApplication {
 
     private final MemberRepository memberRepository;
     private final BoardRepository boardRepository;
+    private final CommentRepository commentRepository;
 
     public static void main(String[] args) {
         SpringApplication.run(SnsApplication.class, args);
@@ -26,18 +29,38 @@ public class SnsApplication {
     public void init() throws InterruptedException {
 
         for (int i = 1; i <= 10; i++) {
-            // member
+            // 회원 생성
             Member member = new Member("member" + i);
             memberRepository.save(member);
 
             for (int j = 1; j <= 5; j++) {
-                // board
+                // 게시글 생성
                 Board board = new Board("title" + j, "content" + j, Category.PUBLIC, member);
                 boardRepository.save(board);
-//                Thread.sleep(500);
             }
         }
 
+        // 댓글 생성
+        Comment parentComment = new Comment();
+
+        memberRepository.findById(1L).ifPresent(parentComment::setMember);
+        boardRepository.findById(1L).ifPresent(parentComment::setBoard);
+
+        parentComment.setContent("부모 댓글 1");
+
+        commentRepository.save(parentComment);
+
+        for (int i = 1; i <= 3; i++) {
+            Comment childComment = new Comment();
+
+            memberRepository.findById(1L).ifPresent(childComment::setMember);
+            boardRepository.findById(1L).ifPresent(childComment::setBoard);
+
+            childComment.setParentComment(parentComment);
+            childComment.setContent("자식 댓글 " + i);
+
+            commentRepository.save(childComment);
+        }
     }
 
 }
