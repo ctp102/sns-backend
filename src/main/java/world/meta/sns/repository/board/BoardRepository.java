72a0d9a8c19f1c4803a7b1df4f7f2c0d7585f786
jh.cho <cjh951002@gmail.com>
2023-04-18 00:20:57 +0900

    fix: 게시글 단건 조회 시 comments는 비즈니스 로직에서 처리하도록 변경

--- src/main/java/world/meta/sns/repository/board/BoardRepository.java
+++ src/main/java/world/meta/sns/repository/board/BoardRepository.java
@@ -7,9 +7,10 @@ import world.meta.sns.entity.Board;
 
 public interface BoardRepository extends JpaRepository<Board, Long>, BoardRepositoryCustom {
 
-    @Query("select b, m, c from Board b " +
+    // comments까지 가지고 오면 성능상 이슈가 있을 것 같아 비즈니스 로직에서 처리
+    @Query("select b, m from Board b " +
             "join fetch b.member m " +
-            "join fetch b.comments c " +
+//            "join fetch b.comments c " +
             "where b.id = :id")
     Board findFetchJoinById(
             @Param("id") Long id
