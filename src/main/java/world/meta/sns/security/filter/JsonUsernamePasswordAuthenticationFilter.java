1be3112c77392a34a555728bfe8f1733880a141b
jh.cho <cjh951002@gmail.com>
2023-05-28 17:20:15 +0900

    feat: 누락된 파일 커밋

---
+++ src/main/java/world/meta/sns/security/filter/JsonUsernamePasswordAuthenticationFilter.java
@@ -0,0 +1,62 @@
+package world.meta.sns.security.filter;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.MediaType;
+import org.springframework.security.authentication.AuthenticationServiceException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.authentication.AbstractAuthenticationProcessingFilter;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+import org.springframework.util.StreamUtils;
+import world.meta.sns.dto.member.MemberLoginDto;
+import world.meta.sns.security.handler.CustomAuthenticationFailureHandler;
+import world.meta.sns.security.handler.CustomAuthenticationSuccessHandler;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+
+/**
+ * form-data 방식이 아닌 JSON 방식으로 로그인을 처리하기 위한 필터
+ */
+public class JsonUsernamePasswordAuthenticationFilter extends AbstractAuthenticationProcessingFilter {
+
+    private static final String DEFAULT_LOGIN_REQUEST_URL = "/api/v1/login";
+    private static final AntPathRequestMatcher DEFAULT_LOGIN_PATH_REQUEST_MATCHER =
+            new AntPathRequestMatcher(DEFAULT_LOGIN_REQUEST_URL, HttpMethod.POST.name());
+
+    private final ObjectMapper objectMapper;
+
+    public JsonUsernamePasswordAuthenticationFilter(ObjectMapper objectMapper, CustomAuthenticationSuccessHandler successHandler, CustomAuthenticationFailureHandler failureHandler) {
+        super(DEFAULT_LOGIN_PATH_REQUEST_MATCHER);
+        super.setAuthenticationSuccessHandler(successHandler);
+        super.setAuthenticationFailureHandler(failureHandler);
+        this.objectMapper = objectMapper;
+    }
+
+    @Override
+    public Authentication attemptAuthentication(HttpServletRequest request, HttpServletResponse response) throws AuthenticationException, IOException {
+        String contentType = request.getContentType();
+        if (StringUtils.isBlank(contentType) || !MediaType.APPLICATION_JSON_VALUE.equals(contentType)) {
+            throw new AuthenticationServiceException("Authentication Content-Type not supported: " + request.getContentType());
+        }
+
+        MemberLoginDto memberLoginDto = objectMapper.readValue(StreamUtils.copyToString(request.getInputStream(), StandardCharsets.UTF_8), MemberLoginDto.class);
+
+        String email = memberLoginDto.getEmail();
+        String password = memberLoginDto.getPassword();
+
+        if (StringUtils.isBlank(email) || StringUtils.isBlank(password)) {
+            throw new AuthenticationServiceException("");
+        }
+
+        UsernamePasswordAuthenticationToken usernamePasswordAuthenticationToken = new UsernamePasswordAuthenticationToken(email, password);
+
+        return super.getAuthenticationManager().authenticate(usernamePasswordAuthenticationToken);
+    }
+
+}
