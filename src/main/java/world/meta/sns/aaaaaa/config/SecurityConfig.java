e52c643e45d1de4fcbdb32c64736dc4d233d0497
jh.cho <cjh951002@gmail.com>
2023-05-28 18:59:38 +0900

    feat: cors 설정 추가

--- src/main/java/world/meta/sns/aaaaaa/config/SecurityConfig.java
+++ src/main/java/world/meta/sns/aaaaaa/config/SecurityConfig.java
@@ -1,4 +1,4 @@
-package world.meta.sns.config;
+package world.meta.sns.aaaaaa.config;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
@@ -19,12 +19,19 @@ import org.springframework.security.oauth2.client.userinfo.OAuth2UserService;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import world.meta.sns.aaaaaa.config.properties.JwtProperties;
 import world.meta.sns.security.filter.JsonUsernamePasswordAuthenticationFilter;
+import world.meta.sns.security.handler.CustomLogoutHandler;
+import world.meta.sns.security.handler.CustomLogoutSuccessHandler;
 import world.meta.sns.security.jwt.JwtUtils;
 import world.meta.sns.security.handler.CustomAuthenticationFailureHandler;
 import world.meta.sns.security.handler.CustomAuthenticationSuccessHandler;
 import world.meta.sns.security.service.CustomOAuth2UserService;
 import world.meta.sns.security.service.CustomUserDetailsService;
+import world.meta.sns.security.service.RedisCacheService;
 
 @Configuration
 @EnableWebSecurity
@@ -32,64 +39,50 @@ import world.meta.sns.security.service.CustomUserDetailsService;
 public class SecurityConfig {
 
     private final ObjectMapper objectMapper;
+
     private final JwtUtils jwtUtils;
+    private final JwtProperties jwtProperties;
     private final BCryptPasswordEncoder bCryptPasswordEncoder;
+
     private final UserDetailsService customUserDetailsService;
     private final OAuth2UserService<OAuth2UserRequest, OAuth2User> customOAuth2UserService;
-
+    private final RedisCacheService redisCacheService;
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
         return httpSecurity
                     .authorizeRequests()
-//                    .antMatchers("/api/v1/**").permitAll()
                     .anyRequest().permitAll()
-//                .and()
-//                    .formLogin()
-//                    .loginPage("/login") // 권한이 없는 페이지에 접근할 때 리다이렉트 시키는 uri(리액트 프론트로 리다이렉트를 하려면?)
-//                    .loginProcessingUrl("/login") // UsernamePasswordAuthenticationFilter의 AntPathRequestMatcher 메서드가 호출될 때 /login인 경우에만 처리한다.
-//                    .usernameParameter("email")
-//                    .passwordParameter("password")
-//                    .successHandler(customAuthenticationSuccessHandler()) // TODO: [2023-05-28] 아래꺼가 적용되면 지우자
-//                    .failureHandler(customAuthenticationFailureHandler())
                 .and()
                     .formLogin().disable()
                     .httpBasic().disable()
                     .csrf().disable()
-                    .addFilterBefore(jsonUsernamePasswordAuthenticationFilter(), UsernamePasswordAuthenticationFilter.class)
-                    .cors()
-                .and()
+                    .addFilterAt(jsonUsernamePasswordAuthenticationFilter(), UsernamePasswordAuthenticationFilter.class)
                     .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                    .cors().configurationSource(corsConfigurationSource())
                 .and()
                     .oauth2Login()
                     .userInfoEndpoint()
                     .userService(customOAuth2UserService)
-//                    .authorizationEndpoint() // ??
-//                    .baseUri("/oauth2/authorization") // ??
-//                    .authorizationRequestRepository() // ??
-//                .and()
-//                    .redirectionEndpoint()
-//                    .baseUri("/*/oauth2/code/*") // ??
                 .and()
                     .successHandler(customAuthenticationSuccessHandler())
                     .failureHandler(customAuthenticationFailureHandler())
                 .and()
                     .logout()
                     .logoutUrl("/logout")
-//                    .deleteCookies("JSESSIONID") ?
-//                    .addLogoutHandler(jwtLogoutHandler())
-//                    .logoutSuccessHandler(jwtLogoutSuccessHandler())
+                    .addLogoutHandler(customLogoutHandler())
+                    .logoutSuccessHandler(customLogoutSuccessHandler())
                 .and()
-//                .and()
-//                    .addFilterBefore()
                 .build();
     }
 
     @Bean
     public JsonUsernamePasswordAuthenticationFilter jsonUsernamePasswordAuthenticationFilter() {
-        JsonUsernamePasswordAuthenticationFilter jsonUsernamePasswordAuthenticationFilter
-                = new JsonUsernamePasswordAuthenticationFilter(objectMapper, customAuthenticationSuccessHandler(), customAuthenticationFailureHandler());
+        JsonUsernamePasswordAuthenticationFilter jsonUsernamePasswordAuthenticationFilter = new JsonUsernamePasswordAuthenticationFilter(objectMapper);
         jsonUsernamePasswordAuthenticationFilter.setAuthenticationManager(authenticationManager());
+        jsonUsernamePasswordAuthenticationFilter.setAuthenticationSuccessHandler(customAuthenticationSuccessHandler());
+        jsonUsernamePasswordAuthenticationFilter.setAuthenticationFailureHandler(customAuthenticationFailureHandler());
         return jsonUsernamePasswordAuthenticationFilter;
     }
 
@@ -103,12 +96,6 @@ public class SecurityConfig {
         return new ProviderManager(provider);
     }
 
-//    @Bean
-//    public PasswordEncoder passwordEncoder() {
-//        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
-//        return bCryptPasswordEncoder;
-//    }
-
     @Bean
     public CustomAuthenticationSuccessHandler customAuthenticationSuccessHandler() {
         return new CustomAuthenticationSuccessHandler(jwtUtils, objectMapper);
@@ -117,6 +104,29 @@ public class SecurityConfig {
     @Bean
     public CustomAuthenticationFailureHandler customAuthenticationFailureHandler() {
         return new CustomAuthenticationFailureHandler(objectMapper);
+    }
+
+    @Bean
+    public CustomLogoutHandler customLogoutHandler() {
+        return new CustomLogoutHandler(jwtUtils, jwtProperties, redisCacheService);
+    }
+
+    @Bean
+    public CustomLogoutSuccessHandler customLogoutSuccessHandler() {
+        return new CustomLogoutSuccessHandler();
+    }
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.addAllowedOriginPattern(CorsConfiguration.ALL);
+        configuration.addAllowedHeader(CorsConfiguration.ALL);
+        configuration.addAllowedMethod(CorsConfiguration.ALL);
+        configuration.setAllowCredentials(true);
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
     }
 
 }
