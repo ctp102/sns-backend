3a1d19fe774e481deefb0ec9c0895632619c28ad
jh.cho <cjh951002@gmail.com>
2023-06-05 01:07:31 +0900

    feat: 응답헤더 설정 로직을 공통 클래스로 뺌

---
+++ src/main/java/world/meta/sns/api/common/utils/ResponseUtils.java
@@ -0,0 +1,18 @@
+package world.meta.sns.api.common.utils;
+
+import org.springframework.http.HttpStatus;
+
+import javax.servlet.http.HttpServletResponse;
+import java.nio.charset.StandardCharsets;
+
+import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
+
+public class ResponseUtils {
+
+    public static void setResponseHeader(HttpServletResponse response, HttpStatus status) {
+        response.setCharacterEncoding(StandardCharsets.UTF_8.name());
+        response.setStatus(status.value());
+        response.setContentType(APPLICATION_JSON_VALUE);
+    }
+
+}
